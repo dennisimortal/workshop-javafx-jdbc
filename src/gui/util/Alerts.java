b1fa7ba36e9638d929882ddb0211e1d25f275722
Denis P Roque <dennisimortal47@gmail.com>
2022-05-05 17:31:02 -0300

    About view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,12 @@
+package gui.util;
+
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+
+	public static void showAlets(String string, String string2, String message, AlertType error) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
